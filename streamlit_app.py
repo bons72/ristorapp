0ed85eee73d0dd80ac763334c202125a2906970d
Riccardo <bons72@gmail.com>
2026-03-01 20:06:02 +0100

    Fix: separato packages.txt da requirements.txt

---
+++ streamlit_app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import pandas as pd
+import sys
+
+st.title("TEST - PALAZZO FIORINI")
+st.write("Python version:", sys.version)
+st.write("Se vedi questo, le dipendenze sono installate!")
